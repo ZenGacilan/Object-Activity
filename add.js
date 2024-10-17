312ce013e41417d567ea9af5704c53ed5a58fee9
ZenGacilan <potapetekau@gmail.com>
2024-10-17 20:49:58 +0800

    initial commit

---
+++ add.js
@@ -0,0 +1,19 @@
+// Create a new JavaScript file named objectActivity.js
+
+// Task 1: Create an object called car
+const car = {
+    type: "Honda",
+    model: "Civic",
+    color: "Red"
+};
+
+// Task 2: Use typeof to check the type of the object
+console.log("Type of car:", typeof car);
+
+// Task 3: Update the type property
+car.type = "Toyota";
+console.log("Updated car object:", car);
+
+// Task 4: Add a new property
+car.wheels = 4;
+console.log("Car object with wheels property:", car);
